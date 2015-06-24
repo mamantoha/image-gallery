7c70dd9b64c9e96a679fa8102c0d65054160f0cb
Anton Maminov <anton.linux@gmail.com>
2015-06-24 18:07:29 +0300

    HTTP basic authentication

--- app.rb
+++ app.rb
@@ -1,5 +1,6 @@
 class App < Sinatra::Base
   get '/' do
+    @auth = authorized?
     @images = Image.all
 
     slim :index
@@ -11,10 +12,32 @@ class App < Sinatra::Base
     slim :show
   end
 
+  get '/auth' do
+    protected!
+
+    redirect '/'
+  end
+
   post '/images' do
+    protected!
+
     @image = Image.new params[:image]
     @image.save
 
     redirect '/'
+  end
+
+  helpers do
+    def protected!
+      return if authorized?
+      headers['WWW-Authenticate'] = 'Basic realm="Restricted Area"'
+      halt 401, "Not authorized\n"
+    end
+
+    def authorized?
+      @auth ||= Rack::Auth::Basic::Request.new(request.env)
+      @auth.provided? && @auth.basic? && @auth.credentials && @auth.credentials == ['admin', 'admin']
+    end
+
   end
 end
